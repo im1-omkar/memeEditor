783e0ab06c63e329431d5a65117535e0228e135e
Omkar <robintheconsistent21@gmail.com>
2026-06-17 15:38:03 +0530

    Initial commit

---
+++ be/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+
+const PORT = process.env.PORT;
+const URL:string = process.env.URL ?? "";
+
+const app = express();
+
+
+app.get("/memes", async (req: express.Request, res:express.Response)=>{
+    
+    //fetch the memes 
+    try{
+        const response = await fetch(URL)
+        const data = await response.json();
+        res.status(200).json(data);
+        return;
+
+    }catch(err){
+        if(err instanceof Error){
+            console.log(err.message);
+        }
+        res.status(500).json({
+            "message" : "internal server error"
+        })
+        return;
+    }
+
+    
+})
+
+app.listen(PORT,()=>{
+    console.log("app is running on port :" + PORT )
+})
